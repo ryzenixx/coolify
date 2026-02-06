ad106536d750621f40c34ba596faee787912923c
ryzenixx <mael.duret@icloud.com>
2026-02-06 23:13:03 +0100

    feat: remove 'Sponsor us' link from the navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -357,20 +357,6 @@
                             Onboarding
                         </a>
                     </li> --}}
-                    <li>
-                        <a title="Sponsor us" class="menu-item" href="https://coolify.io/sponsorships"
-                            target="_blank">
-                            <svg class="text-pink-500 menu-item-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <g fill="none" stroke="currentColor" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-width="2">
-                                    <path d="M19.5 12.572L12 20l-7.5-7.428A5 5 0 1 1 12 6.006a5 5 0 1 1 7.5 6.572" />
-                                    <path
-                                        d="M12 6L8.707 9.293a1 1 0 0 0 0 1.414l.543.543c.69.69 1.81.69 2.5 0l1-1a3.182 3.182 0 0 1 4.5 0l2.25 2.25m-7 3l2 2M15 13l2 2" />
-                                </g>
-                            </svg>
-                            <span class="menu-item-label">Sponsor us</span>
-                        </a>
-                    </li>
                 @endif
                 @if (!isSubscribed() && isCloud() && auth()->user()->teams()->get()->count() > 1)
                     <livewire:navbar-delete-team />
