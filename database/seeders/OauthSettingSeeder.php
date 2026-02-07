c853f931f43b4f8ddad51b1b1b3f50780f7cf22c
ryzenixx <mael.duret@icloud.com>
2026-02-07 20:47:44 +0100

    fix(seeder): prevent data loss during OAuth settings re-seed and add keycloak

--- database/seeders/OauthSettingSeeder.php
+++ database/seeders/OauthSettingSeeder.php
@@ -24,6 +24,7 @@ class OauthSettingSeeder extends Seeder
                 'google',
                 'authentik',
                 'infomaniak',
+                'keycloak',
                 'zitadel',
             ]);
 
@@ -50,10 +51,8 @@ class OauthSettingSeeder extends Seeder
                 $provider->delete();
             });
             $allProviders->each(function ($provider) {
-                $provider = new OauthSetting;
-                $provider->provider = $provider->provider;
-                unset($provider->id);
-                $provider->save();
+                $newProvider = $provider->replicate();
+                $newProvider->save();
             });
 
             foreach ($notFoundProviders as $provider) {
